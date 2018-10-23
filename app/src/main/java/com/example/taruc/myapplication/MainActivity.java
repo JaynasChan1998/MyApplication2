e34515e22435d8984f12aefeb6b9ad40b8089c7c
Chan Ting Foong <chantf-pa16@student.tarc.edu.my>
2018-10-23 09:20:04 +0800

    TEST

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MYOWN","Hello World");
-        Log.v("TEST","TEST");
+        Log.v("TEST","LOL");
     }
 }
